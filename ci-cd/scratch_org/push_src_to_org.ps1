42695bb8b437ac389cc2da3e5a280e3f64bc13bf
jdschleicher <jdschleicher@gmail.com>
2022-08-13 03:04:43 -0400

    script adjustment for property name change and removal of devhub auth

--- ci-cd/scratch_org/push_src_to_org.ps1
+++ ci-cd/scratch_org/push_src_to_org.ps1
@@ -1,10 +1,8 @@
-param($scratch_org_alias)
+param($org_alias)
 
 echo "PUSHING SOURCE TO SCRATCH ORG"
 
-ls
-
-$push_src_to_scratch_result_json = $(sfdx force:source:push --targetusername $scratch_org_alias --loglevel trace --json )
+$push_src_to_scratch_result_json = $(sfdx force:source:push --targetusername $org_alias --loglevel trace --json )
 Write-Host $push_src_to_scratch_result_json
 $push_src_to_scratch_result = $push_src_to_scratch_result_json| ConvertFrom-Json
 $push_src_to_scratch_result.result.status
