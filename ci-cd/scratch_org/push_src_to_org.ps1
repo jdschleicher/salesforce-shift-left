8896cb8be8da4063cf25a90cf4272be0d59b9117
jdschleicher <jdschleicher@gmail.com>
2022-08-13 06:47:52 -0400

    test

--- ci-cd/scratch_org/push_src_to_org.ps1
+++ ci-cd/scratch_org/push_src_to_org.ps1
@@ -2,7 +2,7 @@ param($org_alias)
 
 echo "PUSHING SOURCE TO SCRATCH ORG"
 
-$push_src_to_scratch_result_json = $(sfdx force:source:push --targetusername $org_alias --force --loglevel trace --json )
+$push_src_to_scratch_result_json = $(sfdx force:source:push --targetusername $org_alias -f --loglevel trace --json )
 Write-Host $push_src_to_scratch_result_json
 $push_src_to_scratch_result = $push_src_to_scratch_result_json| ConvertFrom-Json
 $push_src_to_scratch_result.result.status
